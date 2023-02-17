ef9600ee93030918244babedf5fc6438ce8c1cdd
vin-sipoi <vincentsipoi01@gmail.com>
2023-02-16 20:50:27 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,9 @@
  *Description: A C program that prints with a put function
  * 
  *Return: Always 0 (success)
- * */
+ *
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");

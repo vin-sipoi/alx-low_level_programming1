55d66b8b578f7fc318f38aa336dd583f24fb035b
vin-sipoi <vincentsipoi01@gmail.com>
2023-02-16 21:19:16 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,14 +2,14 @@
 
 /**
  * main - Entry point
- * 
- * Description: A C program that prints with put function
- * 
+ *
+ * Description: A C program that prints with printf function.
+ *
  * Return: Always 0 (Success)
- */
+*/
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
